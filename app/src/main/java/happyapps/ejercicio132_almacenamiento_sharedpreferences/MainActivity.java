98b1edd4b4653c38fed87e9435e3d01fa6da77db
Longynus <hacpin@hotmail.com>
2016-07-02 22:58:50 -0500

    Probado y funcionando

---
+++ app/src/main/java/happyapps/ejercicio132_almacenamiento_sharedpreferences/MainActivity.java
@@ -0,0 +1,48 @@
+package happyapps.ejercicio132_almacenamiento_sharedpreferences;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText et1,et2;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        et1=(EditText)findViewById(R.id.et1);
+        et2=(EditText)findViewById(R.id.et2);
+
+    }
+
+    public void Grabar(View view){
+        String nombre=et1.getText().toString();
+        String datos=et2.getText().toString();
+        SharedPreferences preferencias=getSharedPreferences("agenda", Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = preferencias.edit();
+        editor.putString(nombre, datos);
+        editor.commit();
+        Toast.makeText(this,"Datos Guardados", Toast.LENGTH_LONG).show();
+
+    }
+
+    public void recuperar(View view){
+        String nombre=et1.getText().toString();
+        SharedPreferences prefe=getSharedPreferences("agenda",Context.MODE_PRIVATE);
+        String d=prefe.getString(nombre,"");
+
+        if (d.length()==0){
+            Toast.makeText(this, "No hay nombre para recuperar", Toast.LENGTH_SHORT).show();
+        }else{
+            et2.setText(d);
+        }
+
+    }
+}
